1c05fe1e3320665c9a65672eefdd2a540cbe7226
Anacarolina1002 <carolquaglioto@outlook.com>
2020-09-16 14:14:28 -0300

    :octocat: feat: conteúdo

---
+++ operadorsLógicosIF.py
@@ -0,0 +1,31 @@
+#Operadores lógicos que podemos usar com if
+#a == b igual
+#a != b diferente
+#a < b a menor que b
+#a > b a maior que b
+#a <= b a menor ou igual a b
+#a >= b a maior ou igual a b
+
+a = 2
+b = 7
+
+if (a > b):
+    print("A letra A é maior que b") 
+else :
+    print("B é maior que A")
+
+if (a == b):
+    print("A é igual a B")
+else:
+    print("A é diferente de B")
+
+soma = a + b
+if (soma / 2 == 0) :
+    print("O número é divisível por dois")
+else:
+    print("O número não pode ser dividido por dois")
+
+if a < b:
+    print("A é menor que B")
+elif a == b:
+    print("A é igual a B")
