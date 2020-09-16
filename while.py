1ab58f1a2ab1ca50adc1c0b26a5b032f7cb2e40e
Anacarolina1002 <carolquaglioto@outlook.com>
2020-09-16 16:17:23 -0300

    :octocat: feat: while

---
+++ while.py
@@ -0,0 +1,18 @@
+#while usamos enquanto a condição for verdadeira em um loop
+
+i=0;
+while i<6:
+    print("Laço de repetição:",i)
+    i=i+1
+    
+while i<7:
+    print("Loop elegante",i)
+    i=i-1
+    if i==3:
+        break
+    i=i+1
+
+
+
+
+
