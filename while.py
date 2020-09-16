df78e3924ed6845e0afbcba10133d462320b8a40
Anacarolina1002 <carolquaglioto@outlook.com>
2020-09-16 17:00:47 -0300

    :octocat: fix: while

--- while.py
+++ while.py
@@ -1,18 +1,18 @@
-#while usamos enquanto a condição for verdadeira em um loop
+#while usamos enquanto a condição for verdadeira (loop infinito)
+#comentei pq fica infinito, pode zikar teu computador.
+#while i <= 6:
+#    print("loop infinito",i)
 
+#while loop finito (com parada se satisfaz a condição)
 i=0;
 while i<6:
-    print("Laço de repetição:",i)
+    print("loop elegante", i)
     i=i+1
-    
-while i<7:
-    print("Loop elegante",i)
-    i=i-1
-    if i==3:
+
+#while com loop finito e if com break para quebrar o laço de repetição(while)
+x=0
+while x<5:
+    print("loop mais elegante ainda", x)
+    if x==3:
         break
-    i=i+1
-
-
-
-
-
+    x=x+1
