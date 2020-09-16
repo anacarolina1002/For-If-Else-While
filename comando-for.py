ef0c3dea81c2d98befaeafb06a6596c340020d5e
Anacarolina1002 <carolquaglioto@outlook.com>
2020-09-16 16:58:32 -0300

    :octocat: fit: for

--- comando-for.py
+++ comando-for.py
@@ -1,22 +1,34 @@
-#for é um laço ou estrura de alteração
+#for é um laço ou estrutura de iteração
 
-minhaLista = ['Java','JS','Dart','Ruby','Python']
-
+#Percorrer uma lista
+minhaLista = ['Java', 'JS', 'Dart', 'Ruby', 'Python']
 for x in minhaLista:
-    print("Minha linguagem:",x)
+    print("Minha Linguagem: ", x)
 
+#Percorrer uma palavra
 for x in 'Python':
-    print("Letras da palavra:",x)
-
+    print("Letras da palavra: ", x)
+ 
+#Parar de percorrer a lista(sair do loop) quando encontrar a palavra Dart.
 for x in minhaLista:
-    print("Minhas Linguagens:",x)
-    if x == 'Dart':
+    print("Minhas linguagens: ", x)
+    if x=='Dart':
         break
+
+#Usando função range para percorrer o for
 for x in range(5):
-    print("Usando função range():",x)
-for x in range(2,5):
-    print("Usando função range() com ínicio e fim:",x)
-for x in range(2,5,10):
-    print("Usando função range() com ínicio e fim e incremento personalizado:",x)
+    print("Usando função range(): ",x)
+else:
+    print("Acabou a execução do loop for! :) ")
+
+#Usando função range definindo intervalo
+for x in range(2,6):
+    print("Usando função range() definindo início e fim: ",x)
+
+#Usando função range com incremento personalizado
+for x in range(5,50,10):
+    print("Usando função range() definindo início, fim e incremento personalizado: ",x)
+
+#Se criar for sem bloco de código, tem que usar o pass, se não vai dar erro de sintaxe.
 for x in minhaLista:
-    pass #for sem bloco de código senão da erro de sintaxe
+    pass
