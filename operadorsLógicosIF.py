420a8836c90d99bbad48bb9f5e4809935efe62ad
Anacarolina1002 <carolquaglioto@outlook.com>
2020-09-16 16:59:22 -0300

    :octocat: fit: elif

--- operadorsLógicosIF.py
+++ operadorsLógicosIF.py
@@ -1,34 +1,41 @@
-#Operadores lógicos que podemos usar com if
-#a == b igual
-#a != b diferente
-#a < b a menor que b
-#a > b a maior que b
-#a <= b a menor ou igual a b
-#a >= b a maior ou igual a b
+#Operadores Relacionais que podemos usar com if 
+# a==b  igual
+# a!=b  diferente
+# a<b   menor             12345 b=6
+# a<=b  menor igual       123456  b=6
+# a>b   maior 
+# a>=b  maior igual 
 
-a = 2
-b = 7
-
-if (a > b):
-    print("A letra A é maior que b") 
-else :
-    print("B é maior que A")
-
-if (a == b):
-    print("A é igual a B")
+a=2
+b=4
+c=1
+# if com o uso de um operador relacionais, aninhamento (if dentro de if)
+if a<b:
+    print("Sim é menor")
+    c=a
+    print("Valor de c:", c)
+    if c==b:
+        print("Sim é igual")
+    else:
+        print("Não é!")
 else:
-    print("A é diferente de B")
+    print("Não é menor")
 
-soma = a + b
-if (soma / 2 == 0) :
-    print("O número é divisível por dois")
-else:
-    print("O número não pode ser dividido por dois")
+#if ternário ou operador ternário, se dentro do bloco do if tiver apenas uma linha 
+if a<b: print("É menor!")
 
-if a < b:
-    print("A é menor que B")
-elif a == b:
-    print("A é igual a B")
+#if else ternário ou operadores ternários, se dentro do bloco do if e else tiver apenas uma linha
+print("b é maior") if a < b else print ("o b não é maior")
 
-if a != b:
-    pass #pass é o mesmo que passar, para que o if não fique em branco
+#if para fazer mais de uma verificação, com operadores lógicos
+if a<b and c!=a:
+    print("valor de a: ", a)
+    print("valor de b: ", b)
+    print("valor de c: ", c)
+    print("(and) Isso é verdade!")
+elif a<b or c!=a:
+    print("(or) Isso é verdade!")
+
+# não podemos criar if, elif ou else vazio, se precisar criar assim, use o pass.
+if a<b:
+    pass
